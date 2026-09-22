68a90981901db443310e8ced33a3beb33f7cf00d
Dweeb <vaebeut@gmail.com>
2026-09-22 13:09:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const userAgent = navigator.userAgent;
+const isMobile = /Mobi|Android|iPhone|iPad/i.test(userAgent);
+if (isMobile) {
+  window.location.href = "https://rdweeb.github.io/mobile-1/";
+} else {
+  window.location.href = "https://rdweeb.github.io/pc/";
+}
